e1deffe9f017da87b902c42fd01bcf98fdf23507
Charlotte Kies <85890565+charlottekies@users.noreply.github.com>
2023-09-09 20:49:12 -0400

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/drum-roulette',
   images: {
     domains: ["server.pickyourdrum.link"],
     unoptimized: true
